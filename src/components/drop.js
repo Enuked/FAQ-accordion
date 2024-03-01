b77b51a2f36644dcffca8a72a60d62ec0e995fff
Enuked <cenoke102@gmail.com>
2024-03-01 20:56:43 +0800

    border added & button logic fix

--- src/components/drop.js
+++ src/components/drop.js
@@ -1,23 +1,40 @@
-const accordions = document.querySelectorAll('.accordion');
-const plus = "../../public/assets/images/icon-plus.svg";
-const minus = "../../public/assets/images/icon-minus.svg";
+// const accordions = document.querySelectorAll(".accordion");
+// const plus = "../../public/assets/images/icon-plus.svg";
+// const minus = "../../public/assets/images/icon-minus.svg";
 
-accordions.forEach((accordion) => {
-  accordion.addEventListener('click', () => {
-    const body = accordion.querySelector('.drop-down');
-    const img = accordion.querySelector('#img');
+const accordion = document.querySelectorAll(".accordion > button");
+// const img = document.querySelector('.img');
 
-    const activeBodies = document.querySelectorAll('.drop-down.active');
-    activeBodies.forEach(activeBody => {
-      if (activeBody !== body) {
-        activeBody.classList.remove('active');
-        const activeAccordion = activeBody.parentElement;
-        const activeImg = activeAccordion.querySelector('#img');
-        activeImg.src = plus; // Reset all icons to plus when collapsing
-      }
+// if(img){
+//   img.src = plus;
+// }
+
+// accordions.forEach((accordion) => {
+//   accordion.addEventListener("click", () => {
+//     const body = accordion.querySelector(".drop-down");
+//     const img = accordion.querySelector(".img");
+
+//     const activeBodies = document.querySelectorAll(".drop-down.active");
+//     activeBodies.forEach((activeBody) => {
+//       if (activeBody !== body) {
+//         activeBody.classList.remove("active");
+//         const activeAccordion = activeBody.parentElement;
+//         const activeImg = activeAccordion.querySelector("#img");
+//         activeImg.src = plus; // Reset all icons to plus when collapsing
+//       }
+//     });
+
+//     body.classList.toggle("active");
+//     img.src = body.classList.contains("active") ? minus : plus; // Toggle image based on active class
+//   });
+// });
+
+if (accordion) {
+  [...accordion].forEach((event) => {
+    event.addEventListener("click", () => {
+      // console.log(e.target.nodeName === "DIV");
+      console.log(event);
+      event.parentNode.classList.toggle("active");
     });
-
-    body.classList.toggle('active');
-    img.src = body.classList.contains('active') ? minus : plus; // Toggle image based on active class
   });
-});
+}
